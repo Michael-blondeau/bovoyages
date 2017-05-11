935fee3c0564ebd03ff4edf42008fc6f6c792495
Michael-blondeau <color.ui>
2017-05-11 15:11:38 +0200

    message en franglais

--- src/test/bovoyages/Hello.java
+++ src/test/bovoyages/Hello.java
@@ -3,7 +3,7 @@ package test.bovoyages;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Say Hello Git");
+		System.out.println("Say Hello Git yop yop");
 
 	}
 
