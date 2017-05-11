e5140ddd1909947c5641f8cbf59de69a8e198de6
Naiaaphykit <pujol-aurelie@orange.fr>
2017-05-11 16:23:01 +0200

    Update Hello.java

--- src/test/bovoyages/Hello.java
+++ src/test/bovoyages/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Say Hello Git yop yop");
 		System.out.println("Une petite danse??");
+		System.out.println("Vive la Guadeloupe! Vive le soleil! Vive les beaux gosses!");
 
 	}
 
